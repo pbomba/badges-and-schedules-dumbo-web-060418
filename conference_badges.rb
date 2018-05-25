e11c81187d10941ddedbbed1bd6293d7f93a71b5
Paul Bomba <paul@paulbomba.com>
2018-05-25 14:33:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,6 +26,7 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  badgeBatch = batch_badge_creator(array)
+  roomAssign = assign_rooms(array)
+  puts badgeBatch, roomAssign
 end
