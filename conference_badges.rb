41b04fc4e47d69e5736f95dfef03819f32341a72
Paul Bomba <paul@paulbomba.com>
2018-05-25 14:24:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  newArray = []
+  i = 0
+  while i < array.length
+    person = array[i]
+    newArray << "Hello, my name is #{person}."
+    i += 1
+  end
+  return newArray
+end
+
+def assign_rooms(array)
+    newArray = []
+  i = 0
+  while i < array.length
+    person = array[i]
+    roomNo = i + 1
+    newArray <<  "Hello, #{person}! You'll be assigned to room #{roomNo}!"
+    i += 1
+  end
+  return newArray
+end
+
+def printer(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
+end
